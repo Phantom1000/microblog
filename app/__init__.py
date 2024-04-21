b84ae222c871d4b45dcf0542b302cb4c6f7c70c4
Phantom <nikita.dankin10@gmail.com>
2024-04-21 22:17:15 +0300

    Аутентификация, авторизация и регистрация пользователей

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import logging
 from logging.handlers import RotatingFileHandler
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+login.login_message = 'Пожалуйста, авторизуйтесь'
 # handler = RotatingFileHandler()
 handler = RotatingFileHandler('microblog.log')
 handler.setLevel(logging.INFO)
--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+import sqlalchemy as sa
+from app import db
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +11,21 @@ class LoginForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired(message='Введите пароль')])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired(message='Введите имя пользователя')])
+    email = StringField('Email', validators=[DataRequired(message='Введите email'), Email(message='Проверьте email')])
+    password = PasswordField('Пароль', validators=[DataRequired(message='Введите пароль')])
+    password_confirm = PasswordField('Повторите пароль', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Зарегистрироваться')
+
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(User.username == username.data))
+        if user is not None:
+            raise ValidationError('Пользователь с таким именем уже существует')
+
+    def validate_email(self, email):
+        user = db.session.scalar(sa.select(User).where(User.email == email.data))
+        if user is not None:
+            raise ValidationError('Пользователь с таким email уже существует')
